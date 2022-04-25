7dedf35acadb099625acd78a1e82a9bda5cf31f9
lingxuan <lingxuan016@cycu.org.tw>
2022-04-25 21:42:04 +0800

    加入專案檔案。

---
+++ Q01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("請輸入分數0-100");
+        var input = Console.ReadLine();
+        var result = string.Empty;
+        if (int.TryParse(input, out var value))
+        {
+            result = ScoreGrade(value);
+        }
+        else
+        {
+            result = "輸入的好像不是數字耶XD";
+        }
+        Console.WriteLine(result);
+    }
+    static string ScoreGrade(int score)
+    {
+        
+        var grade = string.Empty;
+        if (score < 0 || score > 100)
+        {
+            grade = "超出輸入範圍";
+        }
+        else if (score <= 100 && score >= 90)
+        {
+            grade = "成績優秀";
+        }
+        else if (score <= 90 && score >= 80)
+        {
+            grade = "考試良好";
+        }
+        else if (score <= 80 && score >= 60)
+        {
+            grade = "考試及格";
+        }
+        else if (score < 60)
+        {
+            grade = "下次繼續努力";
+        }
+
+        return grade;
+    }
+}
+
---
+++ Q02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("今天是:" + DateTime.Now.ToString("ddd dd MMM, yyyy"));
+        Console.WriteLine("請輸入今日溫度");
+        var inputTemperature = Console.ReadLine();
+        Console.WriteLine("請輸入今天天氣(天氣好/天氣不好)");
+        var inputWeather = Console.ReadLine();
+
+        var result = string.Empty;
+        if (int.TryParse(inputTemperature, out var temperature))
+        {
+            result = WhatNeedToDo(DateTime.Now, temperature, inputWeather);
+        }
+        else
+        {
+            result = "輸入的好像不是數字耶XD";
+        }
+        Console.WriteLine($"今天要{result}");
+    }
+    static string WhatNeedToDo(DateTime Date, float temperature, string Weather)
+    {
+
+       var toDo = string.Empty;
+       if(Date.DayOfWeek == DayOfWeek.Sunday || Date.DayOfWeek == DayOfWeek.Saturday)
+        {
+            if (temperature >= 30)
+            {
+                toDo = "游泳";
+            }
+            else
+            {
+                toDo = "爬山";
+            }
+        }
+        else
+        {
+            if (Weather == "天氣好")
+            {
+                toDo = "談業務";
+            }
+            else
+            {
+                toDo = "上網查資料";
+            }
+        }
+        return toDo;
+    }
+}
+
