cc639955f584d7d55fbf1f8c8558eef8ebb02b2d
lingxuan <lingxuan016@cycu.org.tw>
2022-04-26 21:14:15 +0800

    加入Q03-Q05的專案

---
+++ Q03/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("請輸入第一個數值");
+        var Input = Console.ReadLine();
+        Console.WriteLine("請輸入第二個數值");
+        var InputTwo = Console.ReadLine();
+        Console.WriteLine("請輸入第三個數值");
+        var InputThree = Console.ReadLine();
+
+        if (float.TryParse(Input, out float result) &&
+           float.TryParse(InputTwo, out float resultTwo) &&
+           float.TryParse(InputThree, out float resultThree))
+        {
+            var MiddleNumber = FindMiddleNumber(result, resultTwo, resultThree);
+            Console.WriteLine($"中間數為：{MiddleNumber}");
+        }
+        else Console.WriteLine("輸入的好像不是數字耶XD");
+
+
+    }
+    static string FindMiddleNumber(float result, float resultTwo, float resultThree)
+    {
+        var number = string.Empty;
+        var value = result.ToString();
+        var valueTwo = resultTwo.ToString();
+        var valueThree = resultThree.ToString();
+        if (result > resultTwo && result < resultThree || result > resultThree && result < resultTwo)
+        {
+            number = value;
+        }
+        else if (resultTwo > resultThree && resultTwo < result || resultTwo > result && resultTwo < resultThree)
+        {
+            number = valueTwo;
+        }
+        else if (result == resultTwo && result > resultThree)
+        {
+            number = valueThree;
+        }
+        else if (result == resultTwo && result < resultThree)
+        {
+            number = value;
+        }
+        else if (result == resultTwo && resultTwo == resultThree)
+        {
+            number = value;
+        }
+        else if (result == resultThree && result > resultTwo)
+        {
+            number = valueTwo;
+        }
+        else if (result == resultThree && result < resultTwo)
+        {
+            number = value;
+        }
+        else if (resultTwo == resultThree && resultTwo > result)
+        {
+            number = value;
+        }
+        else if (resultTwo == resultThree && resultTwo < result)
+        {
+            number = valueTwo;
+        }
+        else
+        {
+            number = valueThree;
+        }
+        return number;
+    }
+}
+
---
+++ Q05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int printNum = 5;
+        for (int i = 1; i <= 100; i ++)
+        {
+            if (i % 3 == 0 && i % 5 != 0)
+            {
+                Console.Write($"{i},");
+                printNum ++;
+                if (printNum % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
+
