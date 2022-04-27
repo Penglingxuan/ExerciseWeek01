0a174c0024da6f9d75eb85f17196523272f4e2c1
lingxuan <lingxuan016@cycu.org.tw>
2022-04-27 21:15:14 +0800

    新增三題練習題

---
+++ Q12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write($"{i}*{j} = {i * j} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Q13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
